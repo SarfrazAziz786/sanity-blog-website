bf416661849f6557236496e36858b4dc9025492a
SarfrazAziz786 <162900723+SarfrazAziz786@users.noreply.github.com>
2025-01-26 20:18:03 +0500

    Update layout.tsx

--- next-sanity-blog/app/(blog)/layout.tsx
+++ next-sanity-blog/app/(blog)/layout.tsx
@@ -69,7 +69,7 @@ export default async function RootLayout({
     <html lang="en" className={`${inter.variable} bg-white text-black`}>
       <body>
         <section className="min-h-screen">
-           <AlertBanner />}
+           <AlertBanner />
           <main>{children}</main>
           <footer className="bg-accent-1 border-accent-2 border-t">
             <div className="container mx-auto px-5">
